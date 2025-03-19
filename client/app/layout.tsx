cad01f292161bede48a7fcb8aac833ad1fc9aa4e
Hugo-walando <hugowalando@gmail.com>
2025-03-19 11:40:09 +0100

    add custom font, custom colors

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { Poppins } from 'next/font/google';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -6,14 +7,19 @@ export const metadata: Metadata = {
   description: 'Echangez vos cartes Pokémon TCG Pocket !',
 };
 
+const poppins = Poppins({
+  weight: ['400', '600', '700'],
+  variable: '--font-poppins',
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body className={` antialiased`}>{children}</body>
+    <html lang='en' className={poppins.className}>
+      <body>{children}</body>
     </html>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,10 +2,22 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      c_whitebackground: '#F2F4F8',
+      c_redalert: '#F25656',
+      c_primarygreen: '#02DD85',
+      c_grayblue: '#A2B0BC',
+      c_darkblue: '#081932',
+      c_darkgray: '#8F9396',
+    },
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-poppins)'],
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
