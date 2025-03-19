f122e9f5c72dd415d382e811b847a8f7427ded7b
Hugo-walando <hugowalando@gmail.com>
2025-03-19 20:53:32 +0100

    add custom colors vars

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,8 @@
 module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      c_whitebackground: '#F2F4F8',
-      c_redalert: '#F25656',
-      c_primarygreen: '#02DD85',
-      c_grayblue: '#A2B0BC',
-      c_darkblue: '#081932',
-      c_darkgray: '#8F9396',
-    },
     extend: {
       fontFamily: {
         sans: ['var(--font-poppins)'],
