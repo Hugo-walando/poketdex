674d149dbfde2d07800a039cd3cc757f6b9740a6
Hugo-walando <hugowalando@gmail.com>
2025-03-20 14:11:27 +0100

    add btn danger & secondary

--- client/app/components/ui/Button.tsx
+++ client/app/components/ui/Button.tsx
@@ -17,11 +17,12 @@ export default function Button({
   return (
     <button
       className={cn(
-        'px-10  py-2 rounded-full font-sans text-lg font-semibold transition-all duration-200',
+        'px-10 py-2 rounded-full font-sans text-lg shadow-btn font-semibold transition-all duration-200',
         variant === 'primary' &&
-          'text-white border-solid border-2 border-white shadow-btn bg-primarygreen hover:bg-opacity-80',
-        variant === 'secondary' && 'bg-grayblue hover:bg-opacity-80',
-        variant === 'danger' && 'bg-redalert hover:bg-opacity-80',
+          'text-white border-solid border-2 border-white  bg-primarygreen hover:bg-opacity-80',
+        variant === 'secondary' && 'bg-white text-darkgray hover:bg-opacity-80',
+        variant === 'danger' &&
+          'bg-white text-redalert border-2 border-redalert outline-2 outline-white border-solid hover:bg-opacity-80',
         fullWidth && 'w-full',
         className,
       )}
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -6,6 +6,8 @@ export default function Home() {
       Home
       <div className='mx-40 my-40 py-[100px] px-[100px] bg-white rounded-md shadow-base'>
         <Button variant='primary'>Connexion</Button>
+        <Button variant='danger'>Supprimer</Button>
+        <Button variant='secondary'>Annuler</Button>
       </div>
     </div>
   );
