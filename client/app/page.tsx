435707209e93fa6b1b033563b8621d167af335c1
Hugo-walando <hugowalando@gmail.com>
2025-03-20 01:24:29 +0100

    add box-shadow var

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      <div className='mx-40 my-40 w-[100px] h-[100px] bg-white rounded-md shadow-base'></div>
+    </div>
+  );
 }
