35d34e05af7abe86c83ba031160fe3469dc4c413
Hugo-walando <hugowalando@gmail.com>
2025-03-24 15:17:01 +0100

    fix: remove component in test causing problem in deployment

--- client/app/test/page.tsx
+++ client/app/test/page.tsx
@@ -1,11 +1,7 @@
 'use client';
 
-import SetFilterDropdown from '../components/ui/SetFilterDropDown';
-
 export default function Test() {
   return (
-    <div className='bg-white rounded-xl shadow-base p-6 mx-auto mt-20 w-[600px]'>
-      <SetFilterDropdown selectedSet='' onSelectSet={() => {}} />
-    </div>
+    <div className='bg-white rounded-xl shadow-base p-6 mx-auto mt-20 w-[600px]'></div>
   );
 }
