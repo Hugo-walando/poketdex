64a58cc1af6bf75f96336c78f4cce19f2988af08
Hugo-walando <hugowalando@gmail.com>
2025-03-25 20:23:59 +0100

    ui: add CardSearchIcon

---
+++ client/app/components/svgs/BoosterIcon.tsx
@@ -0,0 +1,10 @@
+interface BoosterIconProps {
+  className?: string;
+  color?: string;
+}
+
+const BoosterIcon = ({ className = '', color }: BoosterIconProps) => {
+  return <svg></svg>;
+};
+
+export default BoosterIcon;
