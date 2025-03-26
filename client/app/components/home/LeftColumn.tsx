338ca1aa0f1e7984a8144307571a6abf1cad491f
Hugo-walando <hugowalando@gmail.com>
2025-03-26 11:57:42 +0100

    ui: add home comp folder & LeftColumn comp

---
+++ client/app/components/home/LeftColumn.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import SearchBar from '../ui/SearchBar';
+import SetFilterDropdown from '../ui/SetFilterDropDown';
+import RarityFilter from '../ui/RarityFilter';
+import type { Card } from '@/app/types/index';
+
+interface LeftColumnProps {
+  onCardClick: (card: Card) => void;
+}
+
+const mockCard: Card = {
+  id: '123',
+  name: 'Pikachu',
+  img_url: '/testimgs/cards/PikachuEx.png',
+  rarity: 4,
+  set_id: '1',
+  official_id: 113,
+  created_at: '2024-03-01',
+  updated_at: '2024-03-01',
+};
+
+export default function LeftColumn({ onCardClick }: LeftColumnProps) {
+  return (
+    <div className='w-full md:w-2/3 flex flex-col gap-6'>
+      <div className='flex flex-col md:flex-row gap-4'>
+        <SearchBar placeholder='Rechercher une carte...' onSearch={() => {}} />
+        <SetFilterDropdown selectedSets={[]} onToggleSet={() => {}} />
+        <RarityFilter selectedRarities={[]} onToggleRarity={() => {}} />
+      </div>
+
+      <div className='grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 gap-4'>
+        <div
+          className='w-full h-[180px] bg-gray-100 rounded-lg shadow-base hover:cursor-pointer'
+          onClick={() => onCardClick(mockCard)}
+        />
+        <div className='w-full h-[180px] bg-gray-100 rounded-lg shadow-base' />
+        <div className='w-full h-[180px] bg-gray-100 rounded-lg shadow-base' />
+      </div>
+    </div>
+  );
+}
