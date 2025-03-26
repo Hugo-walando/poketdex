13d5fa4aa70e9bc38c6963244c94d235c99763a4
Hugo-walando <hugowalando@gmail.com>
2025-03-26 12:36:11 +0100

    refactor: move mockListedCard into separate file

--- client/app/components/home/LeftColumn.tsx
+++ client/app/components/home/LeftColumn.tsx
@@ -6,61 +6,11 @@ import RarityFilter from '../ui/RarityFilter';
 import type { ListedCard } from '@/app/types/index';
 import ResetFilters from '../ui/ResetFilters';
 import { useEffect, useState } from 'react';
+import { mockListedCards } from '@/app/data/mockListedCards';
 
 interface LeftColumnProps {
   onCardClick: (card: ListedCard) => void;
 }
-
-export const mockListedCards: ListedCard[] = [
-  {
-    duplicate_id: '1',
-    user: {
-      id: 'u1',
-      username: 'Sacha',
-      profile_picture: '/testimgs/avatars/Av1.png',
-    },
-    card: {
-      id: 'c1',
-      name: 'Pikachu',
-      img_url: '/testimgs/cards/PikachuEx.png',
-      rarity: 4,
-      set_id: '1',
-      official_id: 113,
-    },
-  },
-  {
-    duplicate_id: '2',
-    user: {
-      id: 'u2',
-      username: 'Ondine',
-      profile_picture: '/testimgs/avatars/Av2.png',
-    },
-    card: {
-      id: 'c2',
-      name: 'Misty',
-      img_url: '/testimgs/cards/Amonistar.png',
-      rarity: 3,
-      set_id: '2',
-      official_id: 78,
-    },
-  },
-  {
-    duplicate_id: '3',
-    user: {
-      id: 'u3',
-      username: 'Pierre',
-      profile_picture: '/testimgs/avatars/Av3.png',
-    },
-    card: {
-      id: 'c3',
-      name: 'Racaillou',
-      img_url: '/testimgs/cards/Drakarmin.png',
-      rarity: 2,
-      set_id: '1',
-      official_id: 44,
-    },
-  },
-];
 
 export default function LeftColumn({ onCardClick }: LeftColumnProps) {
   const [listedCards, setListedCards] = useState<ListedCard[]>([]);
---
+++ client/app/data/mockListedCards.ts
@@ -0,0 +1,52 @@
+import { ListedCard } from '../types';
+
+export const mockListedCards: ListedCard[] = [
+  {
+    duplicate_id: '1',
+    user: {
+      id: 'u1',
+      username: 'Sacha',
+      profile_picture: '/testimgs/avatars/Av1.png',
+    },
+    card: {
+      id: 'c1',
+      name: 'Pikachu',
+      img_url: '/testimgs/cards/PikachuEx.png',
+      rarity: 4,
+      set_id: '1',
+      official_id: 113,
+    },
+  },
+  {
+    duplicate_id: '2',
+    user: {
+      id: 'u2',
+      username: 'Ondine',
+      profile_picture: '/testimgs/avatars/Av2.png',
+    },
+    card: {
+      id: 'c2',
+      name: 'Misty',
+      img_url: '/testimgs/cards/Amonistar.png',
+      rarity: 3,
+      set_id: '2',
+      official_id: 78,
+    },
+  },
+  {
+    duplicate_id: '3',
+    user: {
+      id: 'u3',
+      username: 'Pierre',
+      profile_picture: '/testimgs/avatars/Av3.png',
+    },
+    card: {
+      id: 'c3',
+      name: 'Racaillou',
+      img_url: '/testimgs/cards/Drakarmin.png',
+      rarity: 2,
+      set_id: '1',
+      official_id: 44,
+    },
+  },
+];
