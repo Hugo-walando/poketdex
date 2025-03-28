3d3c1d79079ef6229eb59ba9735bd1e1ba354ddf
Hugo-walando <hugowalando@gmail.com>
2025-03-28 12:50:19 +0100

    refactor: change mockdata

--- client/app/types/index.ts
+++ client/app/types/index.ts
@@ -44,41 +44,22 @@ export interface WishlistCard {
 export interface Match {
   id: string;
   status: 'pending' | 'accepted' | 'cancelled';
-  created_at: string;
-  user_1: {
+  createdAt: string;
+  user: {
     id: string;
     username: string;
-    profile_picture: string;
-    friend_code: string;
+    profilePicture: string;
+    friendCode: string;
   };
-  user_2: {
-    id: string;
-    username: string;
-    profile_picture: string;
-    friend_code: string;
-  };
-  card_from_user_1: {
-    id: string;
-    name: string;
-    img_url: string;
-    rarity: number;
-    official_id: number;
-    set: {
-      id: string;
-      name: string;
-      img_url: string;
-    };
-  };
-  card_from_user_2: {
-    id: string;
-    name: string;
-    img_url: string;
-    rarity: number;
-    official_id: number;
-    set: {
-      id: string;
-      name: string;
-      img_url: string;
-    };
-  };
+  offeredCard: CardSummary; // Carte que l’autre joueur t’offre
+  requestedCard: CardSummary; // Carte que tu proposes
+}
+
+export interface CardSummary {
+  id: string;
+  name: string;
+  imgUrl: string;
+  rarity: number;
+  officialId: number;
+  setImage: string;
 }
