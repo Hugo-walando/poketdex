9631b7192a69935db9fbbec4a3de4d1bed311e5d
Hugo-walando <hugowalando@gmail.com>
2025-04-01 12:51:23 +0200

    ui: add radix ui to rarity filter

--- client/app/components/ui/RarityFilter.tsx
+++ client/app/components/ui/RarityFilter.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 import { useFilter } from '@/app/context/FilterContext';
 import { rarities, rarityIcons } from '@/app/data/rarities';
 import { ChevronDown, ChevronUp, StarIcon } from 'lucide-react';
@@ -15,61 +16,59 @@ export default function RarityFilter({
   onToggleRarity,
 }: RarityFilterProps) {
   const { openFilter, setOpenFilter } = useFilter();
-
   const isOpen = openFilter === 'rarity';
-
-  const toggleDropdown = () => {
-    setOpenFilter(isOpen ? null : 'rarity');
-  };
-
   const hasSelected = selectedRarities.length > 0;
 
   return (
-    <div className='relative text-left '>
-      <button
-        onClick={toggleDropdown}
-        className='flex items-center gap-2 px-2 sm:px-3 md:px-4 py-2 text-gray-base md:text-gray-xl bg-white rounded-xl shadow-base hover:cursor-pointer'
-      >
-        <StarIcon className='w-4 h-4 md:w-5 md:h-5 text-darkgray fill-darkgray' />
-        Rareté
-        {isOpen ? (
-          <ChevronUp className='w-6 h-6' />
-        ) : (
-          <ChevronDown className='w-6 h-6' />
-        )}
-      </button>
-      {hasSelected && (
-        <span className='absolute top-0 right-0 w-2 h-2 rounded-full bg-primarygreen ring-2 ring-white' />
-      )}
+    <DropdownMenu.Root
+      open={isOpen}
+      onOpenChange={() => setOpenFilter(isOpen ? null : 'rarity')}
+    >
+      <DropdownMenu.Trigger asChild>
+        <button className='relative flex items-center gap-2 px-2 sm:px-3 md:px-4 py-2 text-gray-base md:text-gray-xl bg-white rounded-xl shadow-base hover:cursor-pointer'>
+          <StarIcon className='w-4 h-4 md:w-5 md:h-5 text-darkgray fill-darkgray' />
+          Rareté
+          {isOpen ? (
+            <ChevronUp className='w-6 h-6' />
+          ) : (
+            <ChevronDown className='w-6 h-6' />
+          )}
+          {hasSelected && (
+            <span className='absolute top-0 right-0 w-2 h-2 rounded-full bg-primarygreen ring-2 ring-white' />
+          )}
+        </button>
+      </DropdownMenu.Trigger>
 
-      {isOpen && (
-        <div className='absolute z-10 mt-1 mx-auto w-[300px] bg-white rounded-xl shadow-base p-2 grid grid-cols-2 gap-2'>
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content
+          sideOffset={8}
+          className='z-50 mt-1 mx-auto w-[300px] bg-white rounded-xl shadow-base p-2 grid grid-cols-2 gap-2'
+        >
           {rarities.map((rarity) => (
-            <button
-              key={rarity}
-              onClick={() => {
-                onToggleRarity(rarity);
-              }}
-              className={`w-full p-1 rounded-xl shadow-base flex items-center justify-center transition hover:cursor-pointer
+            <DropdownMenu.Item asChild key={rarity}>
+              <button
+                onClick={() => onToggleRarity(rarity)}
+                className={`w-full p-1 rounded-xl shadow-base flex items-center justify-center transition hover:cursor-pointer
                 ${
                   selectedRarities.includes(rarity)
                     ? 'bg-darkgray inset-shadow-field'
                     : 'bg-white'
                 }`}
-            >
-              <Image
-                src={rarityIcons[rarity]}
-                alt={`Rareté ${rarity}`}
-                width={0}
-                height={0}
-                sizes='100vw'
-                quality={100}
-                className='object-contain w-auto h-[25px]'
-              />
-            </button>
+              >
+                <Image
+                  src={rarityIcons[rarity]}
+                  alt={`Rareté ${rarity}`}
+                  width={0}
+                  height={0}
+                  sizes='100vw'
+                  quality={100}
+                  className='object-contain w-auto h-[25px]'
+                />
+              </button>
+            </DropdownMenu.Item>
           ))}
-        </div>
-      )}
-    </div>
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
   );
 }
