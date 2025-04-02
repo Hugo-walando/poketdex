557c412493b16e45c2c9ca8e9d7edd2526d6b7ca
Hugo-walando <hugowalando@gmail.com>
2025-04-02 13:29:29 +0200

    feat: import components & did base structure

--- client/app/trades/page.tsx
+++ client/app/trades/page.tsx
@@ -1,3 +1,43 @@
-export default function Trades() {
-  return <div>Trades</div>;
+// app/trades/page.tsx
+
+'use client';
+
+import { useState } from 'react';
+import UserSidebar from '../components/trade/UserSideBar';
+import TradeListSection from '../components/trade/TradeListSection';
+
+// Mock temporaire
+const mockUsers = [
+  {
+    id: 'u1',
+    username: 'AshKetchum',
+    profile_picture: '/testimgs/avatars/Av1.png',
+  },
+  {
+    id: 'u2',
+    username: 'Misty',
+    profile_picture: '/testimgs/avatars/Av2.png',
+  },
+];
+
+export default function TradePage() {
+  const [selectedUserId, setSelectedUserId] = useState<string | null>(
+    mockUsers[0].id,
+  );
+
+  return (
+    <div className='flex h-screen'>
+      {/* Sidebar Utilisateurs */}
+      <UserSidebar
+        users={mockUsers}
+        selectedUserId={selectedUserId}
+        onSelectUser={setSelectedUserId}
+      />
+
+      {/* Panneau d’échange */}
+      <TradeListSection />
+
+      {/* Liste d’activité */}
+    </div>
+  );
 }
