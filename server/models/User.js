463a352b7b147da20da7c94162244f682f7a2732
Hugo-walando <hugowalando@gmail.com>
2025-04-05 20:45:00 +0200

    feat: add 2 fields

--- server/models/User.js
+++ server/models/User.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  role: { type: String, default: 'user', enum: ['user', 'admin'] },
-  profile_picture: String,
-  friend_code: { type: String, unique: true },
-}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, unique: true },
+    email: { type: String, required: true, unique: true },
+    role: { type: String, default: 'user', enum: ['user', 'admin'] },
+    profile_picture: { type: String },
+    friend_code: { type: String, unique: true, sparse: true },
+    is_connected: { type: Boolean, default: false },
+    last_seen: { type: Date },
+  },
+  { timestamps: true },
+);
 
 module.exports = mongoose.model('User', userSchema);
