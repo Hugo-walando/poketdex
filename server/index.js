1a402f7b313098ffc7f35d6b954d86fedae5c243
Hugo-walando <hugowalando@gmail.com>
2025-04-05 20:46:00 +0200

    feat: add errorhandler

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(errorHandler());
 
 // Connect to database
 connectDB();
@@ -18,7 +19,7 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/users', userRoutes);
 
 const setRoutes = require('./routes/setRoutes');
+const errorHandler = require('./middlewares/errorHandler');
 app.use('/api/sets', setRoutes);
-
 
 app.listen(PORT, () => console.log(`Serveur démarré sur le port ${PORT}`));
