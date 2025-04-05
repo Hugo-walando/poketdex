5bf915fff1755e58639c1eab59aaed74508223ab
Hugo-walando <hugowalando@gmail.com>
2025-04-05 15:27:23 +0200

    ui: change style of btn

--- client/app/components/ui/Loader.tsx
+++ client/app/components/ui/Loader.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 
 export default function Loader() {
   return (
-    <div className='flex items-center justify-center h-screen w-full'>
+    <div className='flex items-center justify-center h-20 w-20 bg-white shadow-base mx-auto rounded-xl my-6 '>
       <Loader2 className='animate-spin text-primarygreen w-10 h-10' />
     </div>
   );
