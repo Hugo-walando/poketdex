d163449511030339476c505c2fd03e9e7f71667d
Hugo-walando <hugowalando@gmail.com>
2025-04-05 12:33:17 +0200

    feat: add usesession on profile

--- client/app/profile/page.tsx
+++ client/app/profile/page.tsx
@@ -1,3 +1,12 @@
+'use client';
+import { useSession } from 'next-auth/react';
+
 export default function Profile() {
-  return <div>Profile</div>;
+  const { data: session } = useSession();
+
+  if (!session) {
+    return <p className='text-center mt-10'>🔒 Vous devez être connecté</p>;
+  }
+
+  return <div>Bonjour {session?.user?.email}</div>;
 }
