fa487f5e45014ff9fe7579810c17fdaca780e635
Hugo-walando <hugowalando@gmail.com>
2025-04-05 20:59:11 +0200

    feat: add update controller method for user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,11 +1,9 @@
 const User = require('../models/User');
 
-const User = require('../models/User');
-
 const getCurrentUser = async (req, res) => {
   try {
     const userId = req.user.id; // ajouté par le middleware
-    const user = await User.findById(userId).select('-password'); // pas besoin du password
+    const user = await User.findById(userId).select('-password');
 
     if (!user) {
       return res.status(404).json({ message: 'Utilisateur non trouvé' });
@@ -18,15 +16,10 @@ const getCurrentUser = async (req, res) => {
   }
 };
 
-module.exports = {
-  getCurrentUser,
-};
-
 const getUserById = async (req, res) => {
   try {
     const userId = req.params.id;
-
-    const user = await User.findById(userId).select('-password'); // on ne renvoie pas le mot de passe
+    const user = await User.findById(userId).select('-password');
 
     if (!user) {
       return res.status(404).json({ message: 'Utilisateur non trouvé' });
@@ -39,4 +32,33 @@ const getUserById = async (req, res) => {
   }
 };
 
-module.exports = userController;
+const updateUser = async (req, res) => {
+  const { id } = req.params;
+  const updates = req.body;
+
+  try {
+    if (req.user.id !== id) {
+      return res.status(403).json({ message: 'Accès refusé.' });
+    }
+
+    const user = await User.findByIdAndUpdate(id, updates, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: 'Utilisateur non trouvé' });
+    }
+
+    res.status(200).json(user);
+  } catch (err) {
+    console.error('[updateUser error]', err);
+    res.status(500).json({ message: 'Erreur serveur' });
+  }
+};
+
+module.exports = {
+  getCurrentUser,
+  getUserById,
+  updateUser,
+};
