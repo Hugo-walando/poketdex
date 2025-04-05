253af7c0991edbfd950278f5fd7609457c237fdd
Hugo-walando <hugowalando@gmail.com>
2025-04-05 12:40:39 +0200

    feat: add logoutbutton

--- client/app/profile/page.tsx
+++ client/app/profile/page.tsx
@@ -1,12 +1,17 @@
 'use client';
 import { useSession } from 'next-auth/react';
+import LogoutButton from '../components/ui/LogoutButton';
 
 export default function Profile() {
   const { data: session } = useSession();
 
   if (!session) {
     return <p className='text-center mt-10'>🔒 Vous devez être connecté</p>;
+  } else {
+    return (
+      <div>
+        Bonjour {session?.user?.email} <LogoutButton />
+      </div>
+    );
   }
-
-  return <div>Bonjour {session?.user?.email}</div>;
 }
