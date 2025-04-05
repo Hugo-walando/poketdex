60e8454248366e7756fb3745fe5e71c494c31e24
Hugo-walando <hugowalando@gmail.com>
2025-04-05 20:47:36 +0200

    feat: add user routes needed for now

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,10 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-router.post('/', userController.createUser);
-router.get('/', userController.getAllUsers);
-router.get('/:id', userController.getUserById);
-router.put('/:id', userController.updateUser);
-router.delete('/:id', userController.deleteUser);
+router.get('/me', authenticateToken, getCurrentUser);
+router.get('/:id', getUserById);
 
 module.exports = router;
