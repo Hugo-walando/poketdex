b41608acca1270274c1372c2061fe071ec0e8161
Hugo-walando <hugowalando@gmail.com>
2025-04-09 23:02:53 +0200

    feat: add Set fields

--- server/models/Set.js
+++ server/models/Set.js
@@ -1,9 +1,13 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const setSchema = new mongoose.Schema({
-//   name: { type: String, required: true },
-//   color: String,
-//   img_url: String,
-// }, { timestamps: true });
+const setSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    color: { type: String, required: true },
+    card_count: { type: Number, required: true },
+    img_url: String,
+  },
+  { timestamps: true },
+);
 
-// module.exports = mongoose.model('Set', setSchema);
+module.exports = mongoose.model('Set', setSchema);
