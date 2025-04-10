eb0f78f9f3157460c73f4713ab32c90e413824f8
Hugo-walando <hugowalando@gmail.com>
2025-04-10 17:38:34 +0200

    fix: loop cause of update session

--- client/app/hooks/useFetchSets.ts
+++ client/app/hooks/useFetchSets.ts
@@ -12,55 +12,54 @@ const useFetchSets = () => {
   const [error, setError] = useState<string | null>(null);
   const [success, setSuccess] = useState<string | null>(null);
 
-  const fetchSets = async () => {
-    if (!session?.accessToken) {
-      setError('Utilisateur non authentifié');
-      toast.error('Erreur : utilisateur non authentifié');
-
-      return;
-    }
-    setLoading(true);
-    setError(null);
-    setSuccess(null);
-
-    try {
-      const response = await axiosClient.get<Set[]>(
-        `${process.env.NEXT_PUBLIC_API_URL}/api/sets`,
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${session.accessToken}`,
-          },
-          withCredentials: true,
-        },
-      );
-
-      setSets(response.data);
-      await update(); // rafraîchit la session côté front
-      setSuccess('✅ Sets chargés avec succès');
-      toast.success('Sets chargés avec succès');
-    } catch (err) {
-      if (axios.isAxiosError(err) && err.response) {
-        setError(
-          err.response.data?.message || 'Erreur lors du chargement des sets',
-        );
-        toast.error(
-          err.response.data?.message || 'Erreur lors du chargement des sets',
-        );
-      } else {
-        setError('Erreur lors du chargement des sets');
-        toast.error('Erreur lors du chargement des sets');
-      }
-    } finally {
-      setLoading(false);
-    }
-  };
-
   useEffect(() => {
+    const fetchSets = async () => {
+      if (!session?.accessToken) {
+        setError('Utilisateur non authentifié');
+        toast.error('Erreur : utilisateur non authentifié');
+
+        return;
+      }
+      setLoading(true);
+      setError(null);
+      setSuccess(null);
+
+      try {
+        const response = await axiosClient.get<Set[]>(
+          `${process.env.NEXT_PUBLIC_API_URL}/api/sets`,
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${session.accessToken}`,
+            },
+            withCredentials: true,
+          },
+        );
+
+        setSets(response.data);
+        // await update(); // rafraîchit la session côté front
+        setSuccess('✅ Sets chargés avec succès');
+        toast.success('Sets chargés avec succès');
+      } catch (err) {
+        if (axios.isAxiosError(err) && err.response) {
+          setError(
+            err.response.data?.message || 'Erreur lors du chargement des sets',
+          );
+          toast.error(
+            err.response.data?.message || 'Erreur lors du chargement des sets',
+          );
+        } else {
+          setError('Erreur lors du chargement des sets');
+          toast.error('Erreur lors du chargement des sets');
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
     fetchSets();
   }, []);
 
   return { sets, loading, error, success };
 };
-
 export default useFetchSets;
