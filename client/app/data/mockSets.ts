7194f9e1629a65ca3eced8cba7c192a6b270dfa5
Hugo-walando <hugowalando@gmail.com>
2025-04-10 13:51:28 +0200

    fix: missing code property

--- client/app/data/mockSets.ts
+++ client/app/data/mockSets.ts
@@ -3,25 +3,25 @@ import { Set } from '../types';
 export const mockSets: Set[] = [
   {
     id: '1',
+    code: 'A1',
     name: 'Puissance Génétique',
     color: '#FFD700',
     card_count: 226,
-    code: 'A1',
     img_url: '/testimgs/sets/PuissanceGénétique.png',
   },
   {
     id: '2',
+    code: 'A2',
     name: 'Ile Fabuleuse',
     color: '#FF006E',
-    code: 'A2',
     card_count: 86,
     img_url: '/testimgs/sets/IleFabuleuse.png',
   },
   {
     id: '3',
+    code: 'A3',
     name: 'Choc Spacio Temporel',
     color: '#00C2FF',
-    code: 'A3',
     card_count: 178,
     img_url: '/testimgs/sets/ChocSpacioTemporel.png',
   },
