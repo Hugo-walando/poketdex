47fbdc1f996612cd7585ffba796e4aa5d9e3b596
Hugo-walando <hugowalando@gmail.com>
2025-04-17 13:53:14 +0200

    feat: update Match schema, add indexes

--- server/models/Match.js
+++ server/models/Match.js
@@ -1,31 +1,37 @@
 const mongoose = require('mongoose');
 
-const matchSchema = new mongoose.Schema({
-  user_1: { 
-    type: mongoose.Schema.Types.ObjectId, 
-    ref: 'User', 
-    required: true 
+const matchSchema = new mongoose.Schema(
+  {
+    user_1: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    user_2: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    card_offered_by_user_1: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Card',
+      required: true,
+    },
+    card_offered_by_user_2: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Card',
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['pending', 'accepted', 'cancelled', 'completed'],
+      default: 'pending',
+    },
   },
-  user_2: { 
-    type: mongoose.Schema.Types.ObjectId, 
-    ref: 'User', 
-    required: true 
-  },
-  card_offered_by_user_1: { 
-    type: mongoose.Schema.Types.ObjectId, 
-    ref: 'Card', 
-    required: true 
-  },
-  card_offered_by_user_2: { 
-    type: mongoose.Schema.Types.ObjectId, 
-    ref: 'Card', 
-    required: true 
-  },
-  status: { 
-    type: String, 
-    enum: ['pending', 'accepted', 'cancelled'], 
-    default: 'pending' 
-  },
-}, { timestamps: true });
+  { timestamps: true },
+);
+
+matchSchema.index({ user_1: 1 });
+matchSchema.index({ user_2: 1 });
 
 module.exports = mongoose.model('Match', matchSchema);
