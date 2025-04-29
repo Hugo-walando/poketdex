66493a10c92f9ae4b48450705a01ca496e9fd03e
Hugo-walando <hugowalando@gmail.com>
2025-04-29 14:24:00 +0200

    fix: add log

--- client/app/components/trade/TradeItem.tsx
+++ client/app/components/trade/TradeItem.tsx
@@ -18,6 +18,7 @@ interface TradeItemProps {
 
 export default function TradeItem({ trade, selectedUserId }: TradeItemProps) {
   console.log("🔄 Chargement de l'item d'échange");
+  console.log('Selected user ID:', selectedUserId);
   const currentUserId = useUserStore((state) => state.user?.id);
   const {
     acceptTradeRequest,
