5863f2aed4987a06ff86f292ff41b4bf273c0bd6
Hugo-walando <hugowalando@gmail.com>
2025-05-01 22:07:13 +0200

    ui: add sticky filters & searchbar

--- client/app/components/home/LeftColumn.tsx
+++ client/app/components/home/LeftColumn.tsx
@@ -86,47 +86,49 @@ export default function LeftColumn({ onCardClick }: LeftColumnProps) {
     return matchSearch && matchSet && matchRarity && hasSameRarityWishlist;
   });
   return (
-    <div className='w-full md:w-6/10 mb-10 mt-14 md:mt-0 gap-6'>
-      <h1 className='text-dark-base md:text-dark-xl mb-2'>Cartes Listées</h1>
+    <div className='w-full md:w-6/10 mb-10 mt-14 md:mt-0 gap-6 relative'>
+      <div className='sticky top-5 z-10 rounded-xl mb-4 bg-gradient-to-t p-4'>
+        <h1 className='text-dark-base md:text-dark-xl mb-2'>Cartes Listées</h1>
 
-      <SearchBar
-        placeholder='Rechercher une carte...'
-        onSearch={(query) => setSearchQuery(query.toLowerCase())}
-      />
-      <div className='w-full my-6 items-center flex justify-between'>
-        <div className='flex gap-2 md:gap-4'>
-          <FilterDropdownProvider>
-            {sets.length > 0 && (
-              <SetFilterDropdown
-                selectedSets={selectedSets}
-                onToggleSet={toggleSet}
-                sets={sets}
+        <SearchBar
+          placeholder='Rechercher une carte...'
+          onSearch={(query) => setSearchQuery(query.toLowerCase())}
+        />
+        <div className='w-full mt-4 items-center flex justify-between'>
+          <div className='flex gap-2 md:gap-4'>
+            <FilterDropdownProvider>
+              {sets.length > 0 && (
+                <SetFilterDropdown
+                  selectedSets={selectedSets}
+                  onToggleSet={toggleSet}
+                  sets={sets}
+                />
+              )}
+              <RarityFilter
+                selectedRarities={selectedRarities}
+                onToggleRarity={(rarity) =>
+                  setSelectedRarities((prev) =>
+                    prev.includes(rarity)
+                      ? prev.filter((r) => r !== rarity)
+                      : [...prev, rarity],
+                  )
+                }
               />
-            )}
-            <RarityFilter
-              selectedRarities={selectedRarities}
-              onToggleRarity={(rarity) =>
-                setSelectedRarities((prev) =>
-                  prev.includes(rarity)
-                    ? prev.filter((r) => r !== rarity)
-                    : [...prev, rarity],
-                )
-              }
+            </FilterDropdownProvider>
+            <ResetFilters
+              onClick={resetAllFilters}
+              disabled={!hasActiveFilters}
             />
-          </FilterDropdownProvider>
-          <ResetFilters
-            onClick={resetAllFilters}
-            disabled={!hasActiveFilters}
-          />
+          </div>
+          <button
+            onClick={() => refetchListedCards?.()}
+            className='px-3 py-2 bg-primarygreen text-white rounded hover:opacity-90 flex items-center gap-2 hover:cursor-pointer'
+          >
+            Refresh les cartes <RefreshCcw className='w-4 h-4 inline' />
+          </button>
         </div>
-        <button
-          onClick={() => refetchListedCards?.()}
-          className='px-3 py-2 bg-primarygreen text-white rounded hover:opacity-90 flex items-center gap-2 hover:cursor-pointer'
-        >
-          Refresh les cartes <RefreshCcw className='w-4 h-4 inline' />
-        </button>
       </div>
-      <div className='grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 gap-4 relative'>
+      <div className='grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 gap-4'>
         {filteredListedCards.length === 0 ? (
           <p className='text-gray-xl col-span-full text-center mt-10'>
             Aucune carte trouvée avec ces filtres.
