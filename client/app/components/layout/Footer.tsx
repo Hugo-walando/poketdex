e2be35b0f9f511ca24618646323d1b48b89136fa
Hugo-walando <hugowalando@gmail.com>
2025-05-05 17:53:35 +0200

    feat: add footer

---
+++ client/app/components/layout/Footer.tsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <footer className='w-full text-center  text-gray-base py-4 border-t border-gray-200 mt-50 pb-24 px-4'>
+      <p>
+        Toutes les images, noms et données des cartes Pokémon sont la propriété
+        de <strong>The Pokémon Company</strong> et de leurs détenteurs
+        respectifs.
+      </p>
+      <p className='mt-1'>
+        Ce site n’est ni affilié, ni sponsorisé par Nintendo, Game Freak ou The
+        Pokémon Company.
+      </p>
+    </footer>
+  );
+}
