90dd65c20aea8cd860c411c747aba5b4ad5a5d50
Hugo-walando <hugowalando@gmail.com>
2025-05-09 16:04:43 +0200

    fix: add logs

--- server/controllers/tradeRequestController.js
+++ server/controllers/tradeRequestController.js
@@ -75,6 +75,7 @@ const createQuickTradeRequest = async (req, res) => {
     const io = getSocketIO();
     const connectedUsers = getConnectedUsersMap();
     console.log('connectedUsers :', connectedUsers);
+    console.log('ToUserId :', toUserId);
     const receiverSocketId = connectedUsers.get(toUserId);
     console.log('receiverSocketId :', receiverSocketId);
 
