aecb536bba42a474f249c06076c8ce59ccdae9e3
Hugo-walando <hugowalando@gmail.com>
2025-05-17 19:04:47 +0200

    add logs

--- server/routes/pingRoutes.js
+++ server/routes/pingRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+  console.log('🔁 Ping reçu à', new Date().toISOString());
   res.status(200).send('pong');
 });
 
