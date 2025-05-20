895e72210cb1704a7066c3d32a71c6fe51e07cda
Hugo-walando <hugowalando@gmail.com>
2025-05-20 18:23:37 +0200

    feat: add admin page & getServerSession

---
+++ client/app/admin/page.tsx
@@ -0,0 +1,17 @@
+// app/admin/page.tsx
+import { getServerAuthSession } from '@/auth';
+import { redirect } from 'next/navigation';
+
+export default async function AdminPage() {
+  const session = await getServerAuthSession();
+
+  if (!session || session.user.username !== 'admin') {
+    redirect('/');
+  }
+
+  return (
+    <div className='p-4'>
+      <h1 className='text-xl font-bold'>Panneau Admin</h1>
+    </div>
+  );
+}
