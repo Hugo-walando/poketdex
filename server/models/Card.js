a783dc53988999a9ba0fa1f13b855cb45b67dd51
Hugo-walando <hugowalando@gmail.com>
2025-05-24 15:18:19 +0200

    fix: remove uniqueness of official_id

--- server/models/Card.js
+++ server/models/Card.js
@@ -5,7 +5,7 @@ const cardSchema = new mongoose.Schema(
     img_url: { type: String, required: true },
     name: { type: String, required: true },
     rarity: { type: Number, required: true },
-    official_id: { type: String, unique: true },
+    official_id: { type: String },
     set_code: { type: String, required: true },
   },
   { timestamps: true },
