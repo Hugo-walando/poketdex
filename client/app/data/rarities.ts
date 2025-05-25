c7ed42da87b188b4aeba9c17023ac304c19aa4ca
Hugo-walando <hugowalando@gmail.com>
2025-05-25 17:37:06 +0200

    feat: remove useless rarities

--- client/app/data/rarities.ts
+++ client/app/data/rarities.ts
@@ -1,4 +1,4 @@
-export const rarities = [1, 2, 3, 4, 5, 6, 7, 8] as const;
+export const rarities = [1, 2, 3, 4, 5] as const;
 
 export const rarityIcons: Record<(typeof rarities)[number], string> = {
   1: '/rarities/1.png',
@@ -6,7 +6,7 @@ export const rarityIcons: Record<(typeof rarities)[number], string> = {
   3: '/rarities/3.png',
   4: '/rarities/4.png',
   5: '/rarities/5.png',
-  6: '/rarities/6.png',
-  7: '/rarities/7.png',
-  8: '/rarities/8.png',
+  // 6: '/rarities/6.png',
+  // 7: '/rarities/7.png',
+  // 8: '/rarities/8.png',
 };
